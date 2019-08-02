05e796ca576dd320c31ea2e34206c17fa3de1ca7
tcbegley <tomcbegley@gmail.com>
2019-08-02 11:47:55 +0100

    Specify worker connection

--- worker.py
+++ worker.py
@@ -1,9 +1,10 @@
 from rq import Connection, Worker
 
 from dash_rq_demo import app, queue
+from dash_rq_demo.core import conn
 
 if __name__ == "__main__":
     with app.server.app_context():
-        with Connection():
+        with Connection(conn):
             w = Worker([queue])
             w.work()
